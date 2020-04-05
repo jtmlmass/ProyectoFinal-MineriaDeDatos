ae45a9b11a4aef08be55949e255c431c01c3af6e
Tomas Molina <jtml.mass@gmail.com>
2020-04-05 18:14:34 -0400

    Agregar preprocesamiento de Texto

---
+++ Data Preparation/data_prep.py
@@ -0,0 +1,38 @@
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+import re
+import nltk
+
+# We need this dataset in order to use the tokenizer
+nltk.download('punkt')
+
+# Also download the list of stopwords to filter out
+nltk.download('stopwords')
+
+stemmer = PorterStemmer()
+
+
+def process_text(text):
+    # Make all the strings lowercase and remove non alphabetic characters
+    text = re.sub('[^A-Za-z]', ' ', text.lower())
+
+    # Tokenize the text; this is, separate every sentence into a list of words
+    # Since the text is already split into sentences you don't have to call sent_tokenize
+    tokenized_text = word_tokenize(text)
+
+    # Remove the stopwords and stem each word to its root
+    clean_text = [
+        stemmer.stem(word) for word in tokenized_text
+        if word not in stopwords.words('english')
+    ]
+
+    # Remember, this final output is a list of words
+    return clean_text
+
+
+texto = "Coronavirus disease (COVID-19) is an infectious disease caused by a new virus."
+
+
+texto_procesado = process_text(texto)
+print(texto_procesado)

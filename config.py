fbb4b1d3318fb38a966efe43bb39e20459e37123
Tomas Molina <jtml.mass@gmail.com>
2020-04-08 05:55:49 -0400

    Commenting pre processing and config

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
-# Deben ser cambiados por copia del proyecto
+# Must be changed at the first time of using the project
+
+# Route to where the dataset is being saved
 root_path = '/home/tomas/Desktop/Mineria de Datos/ProyectoFinal-MineriaDeDatos/noncomm_use_subset/'
+# Route to where the pre processed papers are being or going to be saved
 root_results_prep = '/home/tomas/Desktop/Mineria de Datos/ProyectoFinal-MineriaDeDatos/preprocessed_papers/'
--- preprocessing.py
+++ preprocessing.py
@@ -7,13 +7,18 @@ import config
 print("Starting Preprocesing")
 data_loader = DataLoader().get_instance()
 
+# Count to say which number of paper it's being pre processed
 count = 1
+# Loading the papers from data set
 files = data_loader.get_original_papers()
+# Delete the contents from the folder which saves the pre processed papers
 delete_content(config.root_results_prep)
 
+# Cicle for paper to paper
 for paper_json in files:
     prep_paper = {}
 
+    # Convert all the sentences in a list of STR sentences
     abstract = []
     for sentence in paper_json["abstract"]:
         abstract.append(" ".join(dp.process_text(sentence["text"])))
@@ -22,13 +27,18 @@ for paper_json in files:
     for sentence in paper_json["body_text"]:
         body_text.append(" ".join(dp.process_text(sentence["text"])))
 
+    # Re asssign the values after formating
     prep_paper['paper_id'] = paper_json['paper_id']
     prep_paper['title'] = " ".join(
         dp.process_text(paper_json['metadata']['title']))
     prep_paper['body_text'] = body_text
     prep_paper['abstract'] = abstract
+
+    # Save the file with it's ID as name in folder passed in config
     save_file(config.root_results_prep,
               paper_json["paper_id"]+".json", str(prep_paper))
+
+    # Informing which paper it's finished
     print(count)
     count += 1
 
